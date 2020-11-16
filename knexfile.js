18f2a75b0c8a0b6a770503bce41964e8b785a29c
Sean Shadle <seandshadle@gmail.com>
2020-11-16 12:44:02 -0600

    fingers crossed

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,7 @@ module.exports = {
     },
     migrations: {
       directory: "./data/migrations",
+  
     },
     seeds: {
       directory: "./data/seeds"
@@ -38,18 +39,20 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: process.env.DATABASE_URL,
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: './data/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './data/seeds',
     }
   }
 
