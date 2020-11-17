caa532388c179848197ee8ad4fb872022ea0a17a
Sean Shadle <seandshadle@gmail.com>
2020-11-17 16:30:37 -0600

    set up tests

--- __tests__/posts.js
+++ __tests__/posts.js
@@ -25,4 +25,18 @@ describe("Posts integration tests", () => {
         expect(res.type).toBe("application/json")
         expect(res.body.title).toBe("Fallen lamp post")
     })
+
+    // it("POST /posts", async () => {
+    //     const res = await supertest(server)
+    //         .post('/posts')
+    //         .send({title: "Fallen lamp post 2.0", 
+    //         description: "A second lamp post has fallen and hit a car",
+    //         user_id: 1, 
+    //         imgSrc: "https://i.dailymail.co.uk/i/pix/2012/11/27/article-2239161-163B4E9E000005DC-124_634x399.jpg"})
+    //     expect(res.statusCode).toBe(201)
+    //     expect(res.type).toBe("application/json")
+    //     expect(res.body.title).toBe("Fallen lamp post 2.0")
+    //     expect(res.body.description).toBe("A second lamp post has fallen and hit a car")
+    //     expect(res.body.user_id).toBe(1)
+    // })
 })
